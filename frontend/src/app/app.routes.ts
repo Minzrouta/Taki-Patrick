10c50e633d5ba957ce331ca2daf4b096b2e1531d
baptiste.durringer <baptiste.durringer@epita.fr>
2026-04-28 13:44:18 +0200

    feat: add /profile route and clickable username in navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MoviesList } from './movies-list/movies-list';
 import { AddMovie } from './add-movie/add-movie';
 import { EditMovie } from './edit-movie/edit-movie';
 import { PanelAdmin } from './panel-admin/panel-admin';
+import { ProfilePage } from './profile/profile';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'movies', component: MoviesList },
     { path: 'add-movie', component: AddMovie },
     { path: 'edit-movie/:id', component: EditMovie },
-    { path: 'admin', component: PanelAdmin }
+    { path: 'admin', component: PanelAdmin },
+    { path: 'profile', component: ProfilePage },
 ];
