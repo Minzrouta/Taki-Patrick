321c4b9a03a073629df310852c3920aae774a008
Lucile Pelou <lucile.pelou@epita.fr>
2026-04-28 09:57:28 +0200

    fix: update front
    fix: update front

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-tp1');
+  protected readonly title = signal('frontend');
 }
