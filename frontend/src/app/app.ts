d81b10c5140615b2cd700778e28ed50108db7a16
bumblebeeyay <heyitsbumblebee@gmail.com>
2026-04-28 12:43:55 +0200

    pipou as FCK

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from "./navbar/navbar";
 import { Home } from "./home/home"
+import { Footer } from "./footer/footer"
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home],
+  imports: [RouterOutlet, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- frontend/src/app/footer/footer.ts
+++ frontend/src/app/footer/footer.ts
@@ -1,39 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AsyncPipe, DatePipe } from '@angular/common';
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [AsyncPipe, DatePipe],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
-  const currentYear = new Date().getFullYear();
+  currentYear = new Date().getFullYear();
 
-  const siteMap = [
-    {
-      title: "PARIS",
-      links: ["Beaugrenelle", "Champs-Elysées", "La Vilette"]
-    },
-    {
-      title: "LYON",
-      links: ["Vaise", "Carré de Soie", "Bellecour"]
-    },
-    {
-      title: "MARSEILLE",
-      links: ["Madeline", "Plan de campagne", "La Joliette"]
-    },
-    {
-      title: "NICE",
-      links: ["Lingostière", "Masséna", "Gare du Sud"]
-    }
-  ];
-
-  const socialLinks = [
-    { icon: <FaFacebook />, url: "#" },
-    { icon: <FaTwitter />, url: "#" },
-    { icon: <FaInstagram />, url: "#" }
+  siteMap = [
+    { title: 'PARIS',     links: ['Beaugrenelle', 'Champs‑Elysées', 'La Vilette'] },
+    { title: 'LYON',      links: ['Vaise', 'Carré de Soie', 'Bellecour'] },
+    { title: 'MARSEILLE', links: ['Madeline', 'Plan de campagne', 'La Joliette'] },
+    { title: 'NICE',      links: ['Lingostière', 'Masséna', 'Gare du Sud'] }
   ];
 }
