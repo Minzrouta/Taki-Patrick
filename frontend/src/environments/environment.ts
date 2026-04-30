76c2769dc35ed4e25b56e4f3cdb519ad0c0a8a32
baptiste.durringer <baptiste.durringer@epita.fr>
2026-04-30 02:46:27 +0200

    enfaite on pouvait les commit mdr

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAQM85xkVOXBkc5-2BDL2GvtDVbmqrX6PE',
+    authDomain: 'takipipou.firebaseapp.com',
+    projectId: 'takipipou',
+    storageBucket: 'takipipou.appspot.com',
+    messagingSenderId: '318928397533',
+    appId: '1:318928397533:web:895805dc85b47ff82e6734',
+  },
+};
